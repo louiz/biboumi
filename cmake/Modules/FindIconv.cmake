fb01f78b0ac840387613bf671e980cead27f8fc0
Florent Le Coz <louiz@louiz.org>
2013-12-08 15:32:14 +0100

    Rewrite the FindIconv module cleanly

--- cmake/Modules/FindIconv.cmake
+++ cmake/Modules/FindIconv.cmake
@@ -1,29 +1,40 @@
-# - Try to find Iconv
-# Once done this will define
+# - Find iconv
+# Find the iconv (character set conversion) library
 #
-# ICONV_FOUND - system has Iconv
-# ICONV_INCLUDE_DIR - the Iconv include directory
-# ICONV_LIBRARIES - Link these to use Iconv
-# ICONV_SECOND_ARGUMENT_IS_CONST - the second argument for iconv() is const
+# This module defines the following variables:
+#   ICONV_FOUND  -  True if library and include directory are found
+# If set to TRUE, the following are also defined:
+#   ICONV_INCLUDE_DIRS  -  The directory where to find the header file
+#   ICONV_LIBRARIES  -  Where to find the library file
+#   ICONV_SECOND_ARGUMENT_IS_CONST  -  The second argument for iconv() is const
 #
-include(CheckCXXSourceCompiles)
+# For conveniance, these variables are also set. They have the same values
+# than the variables above.  The user can thus choose his/her prefered way
+# to way to write them.
+#   ICONV_LIBRARY
+#   ICONV_INCLUDE_DIR
+#
+# This file is in the public domain
 
-IF (ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
-  # Already in cache, be silent
-  SET(ICONV_FIND_QUIETLY TRUE)
-ENDIF (ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
+find_path(ICONV_INCLUDE_DIRS NAMES iconv.h
+  DOC "The iconv include directory")
 
-FIND_PATH(ICONV_INCLUDE_DIR iconv.h)
+find_library(ICONV_LIBRARIES NAMES iconv libiconv libiconv-2 c
+  DOC "The iconv library")
 
-FIND_LIBRARY(ICONV_LIBRARIES NAMES iconv libiconv libiconv-2 c)
+# Use some standard module to handle the QUIETLY and REQUIRED arguments, and
+# set ICONV_FOUND to TRUE if these two variables are set.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Iconv REQUIRED_VARS ICONV_LIBRARIES ICONV_INCLUDE_DIRS)
 
-IF(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
-   SET(ICONV_FOUND TRUE)
-ENDIF(ICONV_INCLUDE_DIR AND ICONV_LIBRARIES)
-
-set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
-set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
-IF(ICONV_FOUND)
+# Check if the prototype is
+# size_t iconv(iconv_t cd, char** inbuf, size_t* inbytesleft,
+#              char** outbuf, size_t* outbytesleft);
+# or
+# size_t iconv (iconv_t cd, const char** inbuf, size_t* inbytesleft,
+#              char** outbuf, size_t* outbytesleft);
+if(ICONV_FOUND)
+  include(CheckCXXSourceCompiles)
   check_cxx_source_compiles("
  #include <iconv.h>
  int main(){
@@ -33,25 +44,12 @@ IF(ICONV_FOUND)
  char* out = 0;
  size_t olen = 0;
  iconv(conv, &in, &ilen, &out, &olen);
- return 0;
- }
-" ICONV_SECOND_ARGUMENT_IS_CONST )
-ENDIF(ICONV_FOUND)
-set(CMAKE_REQUIRED_INCLUDES)
-set(CMAKE_REQUIRED_LIBRARIES)
+ return 0;}"
+ ICONV_SECOND_ARGUMENT_IS_CONST)
 
-IF(ICONV_FOUND)
-  IF(NOT ICONV_FIND_QUIETLY)
-    MESSAGE(STATUS "Found Iconv: ${ICONV_LIBRARIES}")
-  ENDIF(NOT ICONV_FIND_QUIETLY)
-ELSE(ICONV_FOUND)
-  IF(Iconv_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could not find Iconv")
-  ENDIF(Iconv_FIND_REQUIRED)
-ENDIF(ICONV_FOUND)
+# Compatibility for all the ways of writing these variables
+  set(ICONV_LIBRARY ${ICONV_LIBRARIES})
+  set(ICONV_INCLUDE_DIR ${ICONV_INCLUDE_DIRS})
+endif()
 
-MARK_AS_ADVANCED(
-  ICONV_INCLUDE_DIR
-  ICONV_LIBRARIES
-  ICONV_SECOND_ARGUMENT_IS_CONST
-)
+mark_as_advanced(ICONV_INCLUDE_DIRS ICONV_LIBRARIES ICONV_SECOND_ARGUMENT_IS_CONST)
