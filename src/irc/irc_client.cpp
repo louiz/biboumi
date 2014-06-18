a705b9af7b1bbce6b6c94788398a4cff9cad9ec9
Florent Le Coz <louiz@louiz.org>
2014-06-19 01:10:13 +0200

    Remove a duplicate “connection closed” message

--- src/irc/irc_client.cpp
+++ src/irc/irc_client.cpp
@@ -96,7 +96,6 @@ void IrcClient::on_connected()
 void IrcClient::on_connection_close()
 {
   static const std::string message = "Connection closed by remote server.";
-  this->send_gateway_message(message);
   const IrcMessage error{"ERROR", {message}};
   this->on_error(error);
   log_warning(message);
