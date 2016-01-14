d58dcde856aa1885e0eb4a1286eeaa6e4861557b
Florent Le Coz <louiz@louiz.org>
2016-01-14 14:33:49 +0100

    Only activate systemd’s watchdog if we are compiling with systemd
    ref #3152

--- unit/biboumi.service.cmake
+++ unit/biboumi.service.cmake
@@ -1,12 +1,13 @@
 [Unit]
 Description=Biboumi, XMPP to IRC gateway
+Documentation=man:biboumi(1) http://biboumi.louiz.org
 After=network.target
 
 [Service]
 Type=${SYSTEMD_SERVICE_TYPE}
 ExecStart=${CMAKE_INSTALL_PREFIX}/bin/biboumi /etc/biboumi/biboumi.cfg
 ExecReload=/bin/kill -s USR1 $MAINPID
-WatchdogSec=10
+WatchdogSec=${WATCHDOG_SEC}
 Restart=always
 User=nobody
 Group=nobody
