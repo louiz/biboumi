1d6aa89ab9656878689d86bd1aa74140756740fd
louiz’ <louiz@louiz.org>
2016-10-31 01:12:39 +0100

    Test self disco

--- tests/end_to_end/__main__.py
+++ tests/end_to_end/__main__.py
@@ -1623,6 +1623,19 @@ if __name__ == '__main__':
                      partial(send_stanza, "<message from='{jid_one}/{resource_one}' to='{irc_server_one}' type='chat'><body>WHOIS {nick_one}</body></message>"),
                      partial(expect_stanza, "/message[@from='{irc_server_one}'][@type='chat']/body[text()='irc.localhost: {nick_one} ~{nick_one} localhost * {nick_one}']"),
                 ]),
+                Scenario("self_disco_info",
+                [
+                     handshake_sequence(),
+                     partial(send_stanza, "<iq type='get' id='get1' from='{jid_one}/{resource_one}' to='{biboumi_host}'><query xmlns='http://jabber.org/protocol/disco#info'/></iq>"),
+                     partial(expect_stanza,
+                     ("/iq[@type='result']/disco_info:query/disco_info:identity[@category='conference'][@type='irc'][@name='Biboumi XMPP-IRC gateway']",
+                      "/iq/disco_info:query/disco_info:feature[@var='jabber:iq:version']",
+                      "/iq/disco_info:query/disco_info:feature[@var='http://jabber.org/protocol/commands']",
+                      "/iq/disco_info:query/disco_info:feature[@var='urn:xmpp:ping']",
+                      "/iq/disco_info:query/disco_info:feature[@var='urn:xmpp:mam:1']",
+                      "/iq/disco_info:query/disco_info:feature[@var='jabber:iq:version']",
+                      )),
+                ]),
     )
 
     failures = 0
