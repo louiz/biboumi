45f7396c8d30ed37570c4ecdaa886388f9beba3e
louiz’ <louiz@louiz.org>
2017-01-25 19:55:45 +0100

    Cancel the IRC server linger timer when we try to-rejoin a channel on it

--- src/bridge/bridge.hpp
+++ src/bridge/bridge.hpp
@@ -241,6 +241,7 @@ public:
    * configured linger time is expired.
    */
   void quit_or_start_linger_timer(const std::string& irc_hostname);
+  void cancel_linger_timer(const std::string& irc_hostname);
 
 private:
   /**
