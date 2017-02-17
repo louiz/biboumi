9f978cda943619c95a41e5da9047c3cc0dd31555
louiz’ <louiz@louiz.org>
2017-02-17 22:59:16 +0100

    docker: fix the SECRET->PASSWORD in entrypoint.sh

--- docker/biboumi/entrypoint.sh
+++ docker/biboumi/entrypoint.sh
@@ -3,7 +3,7 @@
 sed -i s/BIBOUMI_XMPP_SERVER_IP/${BIBOUMI_XMPP_SERVER_IP:-xmpp}/ /etc/biboumi/biboumi.cfg
 sed -i s/BIBOUMI_HOSTNAME/${BIBOUMI_HOSTNAME:-biboumi.localhost}/ /etc/biboumi/biboumi.cfg
 sed -i s/BIBOUMI_ADMIN/${BIBOUMI_ADMIN:-}/ /etc/biboumi/biboumi.cfg
-sed -i s/BIBOUMI_SECRET/${BIBOUMI_SECRET:-missing_secret}/ /etc/biboumi/biboumi.cfg
+sed -i s/BIBOUMI_PASSWORD/${BIBOUMI_PASSWORD:-missing_password}/ /etc/biboumi/biboumi.cfg
 
 echo "Running biboumi with the following conf: "
 cat /etc/biboumi/biboumi.cfg
