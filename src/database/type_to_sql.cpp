2677ac42e8d2e1cf162fec773a9acb453bef8b9b
louiz’ <louiz@louiz.org>
2017-06-14 10:31:45 +0200

    Fix compilation (many warnings, and a linkage error) with clang++

--- src/database/type_to_sql.cpp
+++ src/database/type_to_sql.cpp
@@ -3,5 +3,6 @@
 template <> const std::string TypeToSQLType<int>::type = "INTEGER";
 template <> const std::string TypeToSQLType<std::size_t>::type = "INTEGER";
 template <> const std::string TypeToSQLType<long>::type = "INTEGER";
+template <> const std::string TypeToSQLType<long long>::type = "INTEGER";
 template <> const std::string TypeToSQLType<bool>::type = "INTEGER";
 template <> const std::string TypeToSQLType<std::string>::type = "TEXT";
--- src/database/type_to_sql.hpp
+++ src/database/type_to_sql.hpp
@@ -4,3 +4,10 @@
 
 template <typename T>
 struct TypeToSQLType { static const std::string type; };
+
+template <> const std::string TypeToSQLType<int>::type;
+template <> const std::string TypeToSQLType<std::size_t>::type;
+template <> const std::string TypeToSQLType<long>::type;
+template <> const std::string TypeToSQLType<long long>::type;
+template <> const std::string TypeToSQLType<bool>::type;
+template <> const std::string TypeToSQLType<std::string>::type;
