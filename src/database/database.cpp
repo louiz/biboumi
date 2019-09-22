05ba076a0ec451a5a67410731f5ea23ec1faaa74
louiz’ <louiz@louiz.org>
2019-09-22 15:38:27 +0200

    Use return {...} instead of make_tuple

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -238,9 +238,9 @@ std::tuple<bool, std::vector<Database::MucLogLine>> Database::get_muc_logs(const
     }
 
   if (paging == Database::Paging::first)
-    return std::make_tuple(complete, result);
+    return {complete, result};
   else
-    return std::make_tuple(complete, std::vector<Database::MucLogLine>(result.crbegin(), result.crend()));
+    return {complete, {result.crbegin(), result.crend()}};
 }
 
 Database::MucLogLine Database::get_muc_log(const std::string& owner, const std::string& chan_name, const std::string& server,
