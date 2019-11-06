7571020243889db72bb0182dbacc84e506afa279
louiz’ <louiz@louiz.org>
2019-11-06 17:47:35 +0100

    Make is_empty work with any integral type

--- src/utils/get_first_non_empty.hpp
+++ src/utils/get_first_non_empty.hpp
@@ -2,8 +2,13 @@
 
 #include <string>
 
+template <typename T>
+bool is_empty(const T& val)
+{
+  return val == 0;
+}
+template <>
 bool is_empty(const std::string& val);
-bool is_empty(const int& val);
 
 template <typename T>
 T get_first_non_empty(T&& last)
