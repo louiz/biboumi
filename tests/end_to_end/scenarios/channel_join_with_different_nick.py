77c134a47b0e794080e424be393d778183fd1f37
louiz’ <louiz@louiz.org>
2020-03-02 23:17:48 +0100

    Demonstrate issue #3423 with e2e tests

--- tests/end_to_end/scenarios/channel_join_with_different_nick.py
+++ tests/end_to_end/scenarios/channel_join_with_different_nick.py
@@ -11,4 +11,10 @@ scenario = (
     send_stanza("<presence from='{jid_one}/{resource_one}' to='#bar%{irc_server_one}/{nick_two}' />"),
     # We must receive a join presence in response, without any nick change (nick_two) must be ignored
     expect_self_join_presence(jid = '{jid_one}/{resource_one}', chan = "#bar", nick = "{nick_one}"),
+
+    # An different resource joins the same channel, with a different nick
+    send_stanza("<presence from='{jid_one}/{resource_two}' to='#foo%{irc_server_one}/{nick_two}' />"),
+    # We must receive a join presence in response, without any nick change (nick_two) must be ignored
+    expect_stanza("/presence[@to='{jid_one}/{resource_two}'][@from='#foo%{irc_server_one}/{nick_one}']"),
+    expect_stanza("/message/subject"),
 )
