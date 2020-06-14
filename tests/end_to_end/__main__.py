e4550d324feba5d054ded16c89a4628ed84454de
louiz’ <louiz@louiz.org>
2020-06-15 00:47:27 +0200

    e2e: do not fail to start if no irc database exist yet

--- tests/end_to_end/__main__.py
+++ tests/end_to_end/__main__.py
@@ -185,7 +185,10 @@ class IrcServerRunner(ProcessRunner):
     def __init__(self):
         super().__init__()
         # Always start with a fresh state
-        os.remove("ircd.db")
+        try:
+            os.remove("ircd.db")
+        except FileNotFoundError:
+            pass
         subprocess.run(["oragono", "mkcerts", "--conf", os.getcwd() + "/../tests/end_to_end/ircd.yaml"])
         self.create = asyncio.create_subprocess_exec("oragono", "run", "--conf", os.getcwd() + "/../tests/end_to_end/ircd.yaml",
                                                      stderr=asyncio.subprocess.PIPE)
